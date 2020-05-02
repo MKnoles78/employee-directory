f34fe3741313a7576a0c23e9dd81ba4ece16edf8
MKnoles <mknoles78@gmail.com>
2020-05-01 22:38:25 -0400

    worked on components

---
+++ src/components/EmployeeDirectory/Item.jsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const Item = (props) => {
+    const styles = {
+        employeePhoto: {
+            maxHeight: 50,
+            maxWidth: 50,
+        },
+    };
+
+    const [employeeImageUrl, setEmployeeImageUrl] = use State(
+        "https://images.dog.ceo/breeds/mountain-bernese/n02107683_3034.jpg"
+    );
+    
+    useEffect(() => {
+        axios
+        .get("https://dog.ceo/api/breeds/image/random")
+        .then((response) => {
+            setEmployeeImageUrl (response.data.message);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+    }, []);
+
+    return (
+    <div className="row">
+        <div className="col">
+            <img
+            src={EmployeeImageUrl}
+            alt="Employee avatar"
+            style={styles.employeePhoto}
+            ></img>
+        </div>
+        <div className="col">{props.id}</div>
+        <div className="col">{props.employee_name}</div>
+        <div className="col">{props.employee_salary}</div>
+        <div className="col">{props.employee_age}</div>
+    </div>
+    );
+};
+
+export default Item;
