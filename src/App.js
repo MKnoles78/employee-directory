78d37d9af1c62dce049c092ca8741263fb7725c1
MKnoles <mknoles78@gmail.com>
2020-05-01 22:27:16 -0400

    build out NavBar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import NavBar from "./components/Shared/NavBar";
+import Home from "./components/Home";
+import EmployeeDirectory from "./containers/EmployeeDirectory";
+// import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <NavBar />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/directory" component={EmployeeDirectory} />
+    </Router>
   );
+
 }
 
 export default App;
